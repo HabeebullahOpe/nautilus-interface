1485e3b7d4c9c36ace0f926875e821cbdeb74580
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2025-03-27 15:19:30 -0600

    Feature/vibe arcade (#97)
    * fix missing links and home layout
    
    * vibe arcade initial

--- src/components/Navbar/constants.ts
+++ src/components/Navbar/constants.ts
@@ -40,8 +40,13 @@ export const navlinks = [
     label: "Wrapped Voi",
     href: "/community-chest",
     type: "sub"
-  }
-];
+  },
+  {
+    label: 'Vibe Arcade',
+    href: '/tools',
+  },
+] as const;
+
 
 export const linkLabels: { [key: string]: string } = {
   "/listings": "Listings",
